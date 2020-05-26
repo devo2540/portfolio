90f5ad482b887b11a7308423f07d67d0fa1710e2
Devo <the.devin.carter@outlook.com>
2020-05-26 18:35:28 -0500

    successful call to firestore

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { flamelinkApp } from '../flamelink/index'
 
 const App = () => {
+  const fetchFlameLinkData = async () => {
+    const homepageData = await flamelinkApp.content.get({ schemaKey: 'homepage' })
+    console.log(homepageData)
+  }
+  
+  useEffect(() => {
+    console.log('App Loaded')
+    fetchFlameLinkData()
+  })
+
   return ( 
     <div>I'm the App!</div>
    );
